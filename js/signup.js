07bb7e5c98ea489a6362483e28dd923f5ccde0d3
Hyojin-Moon <moontough8@gmail.com>
2024-12-27 15:40:41 +0900

    중간merge

--- js/signup.js
+++ js/signup.js
@@ -18,14 +18,15 @@ imageInput.addEventListener('change', function (event) {
     const file = event.target.files[0];
 
     if (file) {
-        //파일 이름 가져오기
-        const fileName = file.name;
-
-        // 이미지 미리보기 경로 설정
-        const filePath = `./image/${fileName}`; // image 폴더 경로
-        imagePreview.src = filePath; // 미리보기 이미지 업데이트
-        imagePreview.style.display = 'block'; // 이미지 표시
-        imageInput.style.display = 'none'; // 파일 선택 태그 숨김
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            // Base64 형식으로 이미지 데이터 저장
+            const fileData = e.target.result;
+            imagePreview.src = fileData; // 미리보기 이미지 업데이트
+            imagePreview.style.display = 'block'; // 이미지 표시
+            imageInput.style.display = 'none'; // 파일 선택 태그 숨김
+        };
+        reader.readAsDataURL(file); // 파일 읽기
     }
 });
 
@@ -114,5 +115,5 @@ submitButton.addEventListener('click', () => {
 
     alert('멤버로 등록되었습니다!');
 
-    resetInputs();
+    window.location.href = 'home.html';
 });
