348ffd90141a61970b0336ee663f205509407029
Youngjin Oh <oyj921205@gmail.com>
2024-12-27 11:20:42 +0900

    로컬 스토리지 데이터 가져오기

--- js/details.js
+++ js/details.js
@@ -3,6 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function openModal() {
         modal.classList.add('active')
+        const username = localStorage.getItem('');
+        const mbti = localStorage.getItem('');
+        const gitHub = localStorage.getItem('');
+        const blog = localStorage.getItem('');
+        const introduction = localStorage.getItem('');
+
+        document.getElementsById('modal-').innerText = username;
+        document.getElementsById('modal-').innerText = mbti;
+        document.getElementsById('modal-').innerText = gitHub;
+        document.getElementsById('modal-').innerText = blog;
+        document.getElementsById('modal-').innerText = introduction;
     }
     function closeModal() {
         modal.classList.remove('active')
