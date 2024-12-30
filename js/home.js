879422867244f0f0bd9b2c5220c1d860391c344b
Jongyeon Kim <tyjysong0712@gmail.com>
2024-12-30 09:58:23 +0900

    display속성으로 말풍선 중앙정렬[종연]

--- js/home.js
+++ js/home.js
@@ -1,8 +1,5 @@
-//DOMContentLoaded 이벤트 리스너를 통해 HTML 문서가 완전히 로드된 후에 스크립트를 실행
 document.addEventListener("DOMContentLoaded", () => {
-    //typeIntro: 한 글자씩 introText를 지정된 introElement에 출력
     const introText = "오남어스를 소개할게!";
-    // detailParts 배열의 각 문장을 대응되는 detailElements에 한 글자씩 출력.
     const detailParts = [
         "5명의 남자와 게임 속 캐릭터인 어몽어스를 결합하여, 하나의 목표를 이루기 위한 모습을 상징적으로 표현해보았어!",
         "어몽어스 게임의 팀워크 정신처럼, 원활한 소통과 협력으로 문제를 해결하고 도전 과제를 극복해 나가는 팀이야!",
@@ -21,17 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
     let introIndex = 0;
     let charIndex = 0;
 
+    //introText의 각 글자를 하나씩 출력하는 함수다.
+    //introIndex가 introIndex문자열의 길이보다 작을 경우 실행. 애니메이션이 끝나면 else 부분으로 넘어간다.
+    ////400밀리초 대기 후에 typeDetail(0) 함수를 호출한다.
     function typeIntro() {
         if (introIndex < introText.length) {
             introElement.parentElement.classList.remove("hide")
             introElement.textContent += introText[introIndex];
             introIndex++;
-            setTimeout(typeIntro, 25); //한 글자가 출력될 때마다 setTimeout으로 딜레이를 줘 애니메이션 효과를 만듦
+            setTimeout(typeIntro, 25);
         } else {
             setTimeout(() => typeDetail(0), 400);
         }
     }
 
+    //detailParts배열에서 part 번째 항목의 각 글자를 출력합니다.
+    //현재 항목 (part)의 출력이 끝나면, charIndex를 초기화하고 다음 항목을 출력합니다.
     function typeDetail(part) {
         if (charIndex < detailParts[part].length) {
             detailElements[part].parentElement.classList.remove("hide")
